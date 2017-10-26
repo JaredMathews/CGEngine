09e57c62fa84c3598ef90a1dc4d79151235f9903
Unknown <JMathews@student.neumont.edu>
2017-10-26 11:02:57 -0600

    Scene04 setup and shader.cpp/h added

---
+++ CGEngine/CGEngine/scene04.cpp
@@ -0,0 +1,195 @@
+#include "stdafx.h"
+#include "scene04.h"
+#include "glm\vec3.hpp"
+#include "glm\gtc\matrix_transform.hpp"
+#include "renderer.h"
+#include "timer.h"
+
+#define PHONG
+
+Scene04::~Scene04()
+{
+}
+
+float positionData3[] =
+{
+	-0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+	0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+	0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+	0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+	-0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+	-0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+
+	-0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+	0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+	0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+	0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+	-0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+	-0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+
+	-0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
+	-0.5f,  0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
+	-0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
+	-0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
+	-0.5f, -0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
+	-0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
+
+	0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
+	0.5f,  0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
+	0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
+	0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
+	0.5f, -0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
+	0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
+
+	-0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
+	0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
+	0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
+	0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
+	-0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
+	-0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
+
+	-0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,
+	0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,
+	0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
+	0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
+	-0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
+	-0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f
+};
+
+float colorData3[] =
+{
+	1.0f, 0.0f, 0.0f,
+	0.0f, 1.0f, 0.0f,
+	0.0f, 0.0f, 1.0f
+};
+
+glm::vec3 vPositionData3[] =
+{
+	glm::vec3(-0.8f, -0.8f, 0.0f),
+	glm::vec3(0.8f, -0.8f, 0.0f),
+	glm::vec3(0.0f,  0.8f, 0.0f),
+};
+
+glm::vec3 vColorData3[] =
+{
+	glm::vec3(1.0f, 0.0f, 0.0f),
+	glm::vec3(0.0f, 1.0f, 0.0f),
+	glm::vec3(0.0f, 0.0f, 1.0f),
+};
+
+glm::vec3 vertexData3[] =
+{
+	glm::vec3(-0.8f, -0.8f, 0.0f),
+	glm::vec3(1.0f,  0.0f, 0.0f),
+	glm::vec3(0.8f, -0.8f, 0.0f),
+	glm::vec3(0.0f,  1.0f, 0.0f),
+	glm::vec3(0.0f,  0.8f, 0.0f),
+	glm::vec3(0.0f,  0.0f, 1.0f),
+	glm::vec3(-0.8f, -0.8f, 0.0f),
+	glm::vec3(1.0f,  0.0f, 0.0f),
+};
+
+enum vboID
+{
+	POSITION,
+	COLOR,
+	VERTEX
+};
+
+bool Scene04::Initialize()
+{
+#ifdef PHONG
+	m_cube.shaderProgram = m_engine->Get<Renderer>()->CreateShaderProgram("..\\Resources\\Shaders\\phong.vert", "..\\Resources\\Shaders\\phong.frag");
+#else
+	m_cube.shaderProgram = m_engine->Get<Renderer>()->CreateShaderProgram("..\\Resources\\Shaders\\light.vert", "..\\Resources\\Shaders\\basic.frag");
+#endif
+
+	m_cube.mxModelViewUniform = glGetUniformLocation(m_cube.shaderProgram, "mxModelView");
+	m_cube.mxNormalUniform = glGetUniformLocation(m_cube.shaderProgram, "mxNormal");
+	m_cube.mxMVPUniform = glGetUniformLocation(m_cube.shaderProgram, "mxMVP");
+
+	m_cube.ambientMaterialUniform = glGetUniformLocation(m_cube.shaderProgram, "ambientMaterial");
+	m_cube.diffuseMaterialUniform = glGetUniformLocation(m_cube.shaderProgram, "diffuseMaterial");
+	m_cube.specularMaterialUniform = glGetUniformLocation(m_cube.shaderProgram, "specularMaterial");
+
+	m_light.positionUniform = glGetUniformLocation(m_cube.shaderProgram, "lightPosition");
+	m_light.colorUniform = glGetUniformLocation(m_cube.shaderProgram, "lightColor");
+
+	glGenBuffers(1, m_vboHandles);
+
+	glBindBuffer(GL_ARRAY_BUFFER, m_vboHandles[POSITION]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(positionData3), positionData3, GL_STATIC_DRAW);
+
+	glGenVertexArrays(1, &m_vaoHandle);
+	glBindVertexArray(m_vaoHandle);
+
+	glEnableVertexAttribArray(0);
+	glEnableVertexAttribArray(1);
+
+	glBindVertexBuffer(0, *m_vboHandles, 0, sizeof(glm::vec3) * 2);
+	glBindVertexBuffer(1, *m_vboHandles, sizeof(glm::vec3), sizeof(glm::vec3) * 2);
+
+	glVertexAttribFormat(0, 3, GL_FLOAT, GL_FALSE, 0);
+	glVertexAttribBinding(0, 0);
+
+	glVertexAttribFormat(1, 3, GL_FLOAT, GL_FALSE, 0);
+	glVertexAttribBinding(1, 1);
+
+	return true;
+}
+
+void Scene04::Render()
+{
+	//m_engine->Update();
+
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	// render code
+	glBindVertexArray(m_vaoHandle);
+	glDrawArrays(GL_TRIANGLES, 0, 36);
+	glBindVertexArray(0);
+
+	glfwSwapBuffers(m_engine->Get<Renderer>()->m_window);
+}
+
+void Scene04::Update()
+{
+	m_rotation = m_rotation + m_engine->Get<Timer>()->FrameTime();
+	glm::mat4 translate = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, 0.0f));
+	glm::mat4 rotate = glm::rotate(glm::mat4(1.0f), m_rotation, glm::vec3(1.0f, 0.0f, 0.0f));
+	glm::mat4 mxModel = translate * rotate;
+
+	glm::vec3 ambientMaterial = glm::vec3(0.2f, 0.2f, 0.2f);
+	glUniform3fv(m_cube.ambientMaterialUniform, 1, &ambientMaterial[0]);
+
+	glm::mat4 mxView = glm::lookAt(glm::vec3(0.0f, 1.0f, 1.5f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+
+	glm::mat4 mxProjection = glm::perspective(90.0f, (float)m_engine->Get<Renderer>()->m_width / (float)m_engine->Get<Renderer>()->m_height, 0.01f, 10000.0f);
+
+	glm::mat4 mxModelView = mxView * mxModel;
+	glUniformMatrix4fv(m_cube.mxModelViewUniform, 1, GL_FALSE, &mxModelView[0][0]);
+
+	glm::mat4 mxMVP = mxProjection * mxView * mxModel;
+	glUniformMatrix4fv(m_cube.mxMVPUniform, 1, GL_FALSE, &mxMVP[0][0]);
+
+	glm::mat3 mxNormal = glm::mat3(mxModelView);
+	mxNormal = glm::inverse(mxNormal);
+	mxNormal = glm::transpose(mxNormal);
+	glUniformMatrix3fv(m_cube.mxNormalUniform, 1, GL_FALSE, &mxNormal[0][0]);
+
+	glm::vec3 lightPosition = mxView * glm::vec4(2.0f, 10.0f, 10.0f, 1.0f);
+	glUniform4fv(m_light.positionUniform, 1, &lightPosition[0]);
+
+	glm::vec3 lightColor = glm::vec3(1.0f, 1.0f, 1.0f);
+	glUniform3fv(m_light.colorUniform, 1, &lightColor[0]);
+
+	glm::vec3 diffuseMaterial = glm::vec3(0.0f, 0.0f, 1.0f);
+	glUniform3fv(m_cube.diffuseMaterialUniform, 1, &diffuseMaterial[0]);
+
+	glm::vec3 specularMaterial = glm::vec3(1.0f, 0.0f, 0.0f);
+	glUniform3fv(m_cube.specularMaterialUniform, 1, &specularMaterial[0]);
+}
+
+void Scene04::Shutdown()
+{
+}
---
+++ CGEngine/CGEngine/scene04.h
@@ -0,0 +1,48 @@
+#pragma once
+#include "scene.h"
+class Scene04 :
+	public Scene
+{
+public:
+	Scene04(Engine* engine) : Scene(engine) {};
+	~Scene04();
+
+	bool Initialize();
+	void Render();
+	void Update();
+	void Shutdown();
+
+private:
+	struct object
+	{
+		// id / handles
+		GLuint shaderProgram;
+		GLuint vaoHandle;
+
+		// transforms
+		GLint mxModelViewUniform;
+		GLint mxMVPUniform;
+		GLint mxNormalUniform;
+
+		// material
+		GLint ambientMaterialUniform;
+		GLint diffuseMaterialUniform;
+		GLint specularMaterialUniform;
+	};
+
+	struct light
+	{
+		GLint positionUniform;
+		GLint colorUniform;
+	};
+
+private:
+	GLuint m_vboHandles[3];
+	GLuint m_vaoHandle;
+
+	object m_cube;
+	light m_light;
+
+	float m_rotation;
+};
+
